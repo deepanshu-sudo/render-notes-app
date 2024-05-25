3b5c81c83a2180cef14b2dc7a88c4346873d5048
deepanshu <deepanshu2307696@st.jmi.ac.in>
2024-05-26 02:19:13 +0530

    Added some validation

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 // adding a new resource
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if (!body.content) {
@@ -66,21 +66,22 @@ app.post('/api/notes', (request, response) => {
         important: Boolean(body.important) || false,
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 // toggle the importance of a note
 app.put('/api/notes/:id', (request,response,next)=> {
-    const body = request.body
+    const { content, important } = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    Note.findByIdAndUpdate(
+        request.params.id, 
+        {content, important},
+        {new: true, runValidators: true, context: 'query'}
+    )
         .then(updatedNote => {
             response.json(updatedNote)
         })
@@ -98,6 +99,8 @@ const errorHandler = (error, request, response, next) => {
 
     if(error.name === 'CastError') {
         return response.status(400).send({error: 'malformed id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
