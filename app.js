2dc708baa3f72035a89573bcc5e2e521b71e9ce0
deepanshu <deepanshu2307696@st.jmi.ac.in>
2024-05-26 20:01:26 +0530

    Refactored the App in Separate Modules

---
+++ app.js
@@ -0,0 +1,35 @@
+// configure imports
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+// create a connection
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URL)
+
+mongoose.connect(config.MONGODB_URL)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB:',error.message)
+  })
+
+// set middleware order
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/notes', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
